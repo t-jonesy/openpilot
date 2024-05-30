db946107cfd1f71e567f9e01e0d3120834938fa6
Lukas <61192133+lukasloetkolben@users.noreply.github.com>
2024-05-30 21:13:56 +0200

    Update fingerprints.py

--- selfdrive/car/tesla/fingerprints.py
+++ selfdrive/car/tesla/fingerprints.py
@@ -28,7 +28,8 @@ FW_VERSIONS = {
       b'TeM3_E014p10_0.0.0 (16),Y002.18.00',
       b'TeM3_ES014p11_0.0.0 (25),YS002.19.0',
       b'TeMYG4_DCS_Update_0.0.0 (13),Y4002.27.1',
-      b'TeMYG4_DCS_Update_0.0.0 (9),Y4P002.25.0'
+      b'TeMYG4_DCS_Update_0.0.0 (9),Y4P002.25.0',
+      b'TeMYG4_DCS_Update_0.0.0 (13),Y4P002.27.1'
     ],
     (Ecu.engine, 0x606, None): [
       b'\x01\x00\x05\x1cO\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x002\x12',
