254dabe3ceb9a559d39d5b66cc6d5a7cd4a34223
Lukas <61192133+lukasloetkolben@users.noreply.github.com>
2024-11-18 11:39:49 +0100

    Update carcontroller.py

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -85,7 +85,7 @@ class CarController(CarControllerBase):
       state = 4 if not pcm_cancel_cmd else 13  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
       accel = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
       cntr = CS.das_control["DAS_controlCounter"]
-      if frogpilot_toggles.conditional_tacc and CC.hudControl.leadVisible and  CC.longActive:
+      if frogpilot_toggles.conditional_tacc:
         can_sends.append(self.tesla_can.stock_longitudinal(state, accel, CS.das_control, cntr, CS.out.vEgo))
       else:
         can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CC.longActive))
